124d5f060092b5ea70917663169fc9e347b82e30
hking21 <54800997+hking21@users.noreply.github.com>
2019-09-18 09:14:30 +0100

    delete dialog box
    - added classes for dialog boxes
    - added delete command to delete button

---
+++ Test/Gui/DialogBoxes.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter.messagebox import *
+
+
+class Delete:
+    def __init__(self):
+        if askyesno('Delete', 'Do you want to delete this booking?'):
+            showinfo('Yes', 'Booking Deleted')
+        else:
+            showinfo('No', 'Booking Not Deleted')
+
+
+class Update:
+    def __init__(self):
+        if askyesno('Save Changes', 'Do you want to save changes to this booking?'):
+            showinfo('Yes', 'Changes Saved')
+        else:
+            showinfo('No', 'Changes Not Saved')
+
+
+# B1 = Button(top, text="Delete Booking", command=delete)
+# B1.place(x=50, y=50)
+#
+# B2 = Button(top, text="Update Booking", command=update)
+# B2.place(x=50, y=100)
+#
+# top.mainloop()
